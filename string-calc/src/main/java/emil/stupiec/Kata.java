b4222228dad43ed1cab6350b5eefe8efb120a0b8
Emil Stupiec <emil.stupiec@gmail.com>
2011-07-04 22:46:34 +0200

    Kata task 2

--- string-calc/src/main/java/emil/stupiec/Kata.java
+++ string-calc/src/main/java/emil/stupiec/Kata.java
@@ -10,8 +10,10 @@ public class Kata {
 			sum=0;
 		}else if(numbers.contains(",")){
 			String numbers_splited[]=numbers.split(",");
-			if(numbers_splited.length==2){
-				sum=Integer.parseInt(numbers_splited[0])+Integer.parseInt(numbers_splited[1]);
+			if(numbers_splited.length>=2){
+				sum=0;
+				for(String s: numbers_splited)
+					sum+=Integer.parseInt(s);
 			}
 		}else{
 			sum=Integer.parseInt(numbers);
