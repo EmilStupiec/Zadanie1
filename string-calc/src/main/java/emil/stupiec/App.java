4f005db91233546af4654c03b53a292a20a8c82a
Emil Stupiec <emil.stupiec@gmail.com>
2011-07-04 15:40:02 +0200

    Kata task 1

--- string-calc/src/main/java/emil/stupiec/App.java
+++ string-calc/src/main/java/emil/stupiec/App.java
@@ -6,8 +6,14 @@ package emil.stupiec;
  */
 public class App 
 {
+	private static Kata kata=new Kata();
+	
+	public static Integer add(String numbers){
+		return kata.add(numbers);
+	}
+	
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	System.out.println(add(args[0]));
     }
 }
--- string-calc/src/main/java/emil/stupiec/Kata.java
+++ string-calc/src/main/java/emil/stupiec/Kata.java
@@ -1,13 +1,21 @@
 package emil.stupiec;
 
 public class Kata {
-	private Integer a=null;
 	
 	Kata(){}
 	
 	Integer add(String numbers){
-		
-		
-		return a; 
+		Integer sum=null;
+		if(numbers.isEmpty()){
+			sum=0;
+		}else if(numbers.contains(",")){
+			String numbers_splited[]=numbers.split(",");
+			if(numbers_splited.length==2){
+				sum=Integer.parseInt(numbers_splited[0])+Integer.parseInt(numbers_splited[1]);
+			}
+		}else{
+			sum=Integer.parseInt(numbers);
+		}
+		return sum; 
 	}
 }
