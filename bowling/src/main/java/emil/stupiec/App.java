2c50bd5182717855b534bc23ee17025f7fb8e9c6
Emil Stupiec <emil.stupiec@gmail.com>
2011-07-06 16:18:49 +0200

    bowling roll

--- bowling/src/main/java/emil/stupiec/App.java
+++ bowling/src/main/java/emil/stupiec/App.java
@@ -8,5 +8,10 @@ public class App
 {
     public static void main( String[] args ){
     	Game game=new Game();
+    	for(int i=0;i<10;i++){
+    		game.roll(0);
+    		game.roll(game.getPins_knocked());
+    	}
+    	System.out.println("The game:\n"+game.toString());
     }
 }
--- bowling/src/main/java/emil/stupiec/Game.java
+++ bowling/src/main/java/emil/stupiec/Game.java
@@ -3,18 +3,21 @@ package emil.stupiec;
 import java.util.Random;
 
 public class Game {
-	private Frame frames[];
+	private Frame[] frames;
 	private Integer actual_frame=null;
+	private Integer pins_knocked=null;
 	Game(){
-		this.frames=new Frame[10];
-		this.frames[9].setFinal_frame(true);
+		frames=new Frame[10];
 		actual_frame=0;
+		pins_knocked=0;
+		frames[9].setFinal_frame(true);
 	}
 
 	public void roll(Integer pins){
 		if(pins!=10){
 			Random generator = new Random();
 			Integer score=generator.nextInt(11-pins);
+			pins_knocked=score;
 			switch(frames[actual_frame].getTry_number()){
 				case 0:	frames[actual_frame].setScore_try1(score);
 						break;
@@ -23,6 +26,7 @@ public class Game {
 				case 2: frames[actual_frame].setScore_try3(score);
 						break;
 			}
+			
 			if(score==10 && frames[actual_frame].getTry_number()==0)
 				frames[actual_frame].setStrike(true);
 			else if(score==10 && frames[actual_frame].getTry_number()==1)
@@ -38,6 +42,9 @@ public class Game {
 		Integer score=null;
 		
 		return score;
+	}
+	public Integer getPins_knocked(){
+		return pins_knocked;
 	}
 	public String toString(){
 		String s="";
